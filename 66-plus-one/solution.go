ae5f95d3d242aec88283a98478af2b21834f9213
Avinash Narasimhan <avinash@circles.asia>
2024-05-11 17:57:57 +0800

    added the first leetcode solution

---
+++ 66-plus-one/solution.go
@@ -0,0 +1,20 @@
+package main
+
+func plusOne(digits []int) []int {
+	length := len(digits)
+	if digits[length-1] < 9 {
+		digits[length-1] = digits[length-1] + 1
+		return digits
+	}
+
+	for i := length - 1; i >= 0; i-- {
+		if digits[i] == 9 && (i-1) >= 0 && digits[i-1] < 9 {
+			digits[i] = 0
+			digits[i-1] = digits[i-1] + 1
+			return digits
+		}
+
+	}
+
+	return digits
+}
