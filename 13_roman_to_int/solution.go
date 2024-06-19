56dd263d862b9aaf2737bf16c2c3aa0d6d0ceb17
Avinash Narasimhan <avinash140294@gmail.com>
2024-06-19 09:47:56 +0800

    roman to int

---
+++ 13_roman_to_int/solution.go
@@ -0,0 +1,25 @@
+package main
+
+func romanToInt(s string) int {
+	var current, left, total int
+	romanIntMap := map[uint8]int{
+		'I': 1,
+		'V': 5,
+		'X': 10,
+		'L': 50,
+		'C': 100,
+		'D': 500,
+		'M': 1000,
+	}
+	for i := len(s) - 1; i >= 0; i-- {
+		current = romanIntMap[s[i]]
+		if current < left {
+			total = total - current
+		} else {
+			total = total + current
+		}
+		left = current
+	}
+
+	return total
+}
